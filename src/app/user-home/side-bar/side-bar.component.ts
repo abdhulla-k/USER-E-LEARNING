5683c7ace6954d7d13180a5d1893cfba4cf8de30
abdhulla-k <abdhullak149@gmail.com>
2023-02-08 12:26:33 +0530

    created a div to show some routes in side bar

--- src/app/user-home/side-bar/side-bar.component.ts
+++ src/app/user-home/side-bar/side-bar.component.ts
@@ -4,6 +4,8 @@ import { Store } from '@ngrx/store';
 import { AuthService } from 'src/app/auth/auth.service';
 import { ControllerService } from '../../controller.service';
 import * as fromApp from '../../store/app.reducer';
+import * as AuthActions from '../../auth/store/auth.actions';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
@@ -13,8 +15,14 @@ import * as fromApp from '../../store/app.reducer';
 export class SideBarComponent implements OnInit {
   toggle = false; // to controll the side bar
   loggedIn = false;
+  profile = false;
 
-  constructor( private controllService: ControllerService, private authService: AuthService, private store: Store<fromApp.AppState>) {}
+  constructor(
+    private controllService: ControllerService,
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.controllService.sideNavToggler.subscribe(() => {
@@ -26,11 +34,23 @@ export class SideBarComponent implements OnInit {
     this.store.select('authentication').subscribe(state => {
       this.loggedIn = state.loggedInStatus;
     })
-    
+
   }
 
   // to closs the side bar
   closeNav() {
     this.toggle = false;
+    this.profile = false;
+  }
+
+  openProfile() {
+    this.profile = !this.profile;
+    console.log(this.profile);
+  }
+
+  logOut() {
+    this.store.dispatch(new AuthActions.Logout());
+    this.closeNav();
+    this.router.navigate(['/user/'])
   }
 }
